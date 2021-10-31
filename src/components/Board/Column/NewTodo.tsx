4628499fcef230ca247866224688f4254d434891
Gergő Radeczki <g.radeczki@gmail.com>
2021-10-31 18:05:10 +0100

    added better new todo addition dialog

--- src/components/Board/Column/NewTodo.tsx
+++ src/components/Board/Column/NewTodo.tsx
@@ -1,6 +1,13 @@
 import {
     Box,
-    Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    MenuItem,
+    Stack,
+    TextField
 } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
@@ -14,18 +21,41 @@ interface NewTodoStates {
     isDialogOpen: boolean
     name?: string
     description?: string
+    deadline: Date
+    state: TodoState
 }
+
+const states = [
+    {
+        value: TodoState.PendingState,
+        label: 'Függőben',
+    },
+    {
+        value: TodoState.ProgressState,
+        label: 'Folyamatban',
+    },
+    {
+        value: TodoState.DoneState,
+        label: 'Kész',
+    },
+    {
+        value: TodoState.PostponedState,
+        label: 'Elhalasztva',
+    },
+];
 
 class NewTodo extends React.Component<NewTodoProps, NewTodoStates> {
     constructor(props: any) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleDialogClose = this.handleDialogClose.bind(this);
-        this.handleDialogOK = this.handleDialogOK.bind(this);
+        super(props)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleDialogClose = this.handleDialogClose.bind(this)
+        this.handleDialogOK = this.handleDialogOK.bind(this)
         this.state = {
             isDialogOpen: false,
             name: "",
-            description: ""
+            description: "",
+            deadline: new Date(),
+            state: TodoState.PendingState
         }
     }
 
@@ -60,20 +90,49 @@ class NewTodo extends React.Component<NewTodoProps, NewTodoStates> {
                 <Dialog open={this.state.isDialogOpen} onClose={this.handleDialogClose}>
                     <DialogTitle>Új TODO felvétele</DialogTitle>
                     <DialogContent>
-                        <DialogContentText>Add meg az új TODO adatait.</DialogContentText>
                         <Box component="form" sx={{'& > :not(style)': { m: 1, width: '25ch' },}}>
-                            <TextField
-                                id="todo-name"
-                                label="Név"
-                                variant="outlined"
-                                onChange={(e) => this.setState({name: e.target.value})}
-                            />
-                            <TextField
-                                id="todo-description"
-                                label="Leírás"
-                                variant="outlined"
-                                onChange={(e) => this.setState({description: e.target.value})}
-                            />
+                            <Stack spacing={2}>
+                                <TextField
+                                    id="todo-name"
+                                    label="Név"
+                                    variant="outlined"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    onChange={(e) => this.setState({name: e.target.value})}
+                                />
+                                <TextField
+                                    id="todo-description"
+                                    label="Leírás"
+                                    variant="outlined"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    onChange={(e) => this.setState({description: e.target.value})}
+                                />
+                                <TextField
+                                    id="todo-deadline"
+                                    label="Határidő"
+                                    type="datetime-local"
+                                    defaultValue={this.state.deadline.toISOString().substring(0, 16)}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <TextField
+                                    id="todo-state"
+                                    select
+                                    label="Select"
+                                    value={this.state.state}
+                                    onChange={(e) => this.setState({state: parseInt(e.target.value)})}
+                                >
+                                    {states.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                    ))}
+                                </TextField>
+                            </Stack>
                         </Box>
                     </DialogContent>
                     <DialogActions>
