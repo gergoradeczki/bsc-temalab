83860ff4c9a6ab3771c518c3081613f34f844725
Gergő Radeczki <g.radeczki@gmail.com>
2021-11-03 19:36:25 +0100

    Merged backend and fronted code base

---
+++ backend/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoBackend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
