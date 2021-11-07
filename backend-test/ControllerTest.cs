acd7861e7b51c1520000c3b99b81f82d62f95c5e
Gergő Radeczki <g.radeczki@gmail.com>
2021-11-07 23:15:37 +0100

    Added unit test project

---
+++ backend-test/ControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using temalabor_2021.Data;
+using temalabor_2021.Models;
+
+namespace temalabor_2021.Tests
+{
+    public class ControllerTest
+    {
+        protected ControllerTest(DbContextOptions<AppDbContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+
+            Seed();
+        }
+
+        protected DbContextOptions<AppDbContext> ContextOptions { get; }
+
+        private void Seed()
+        {
+            using (var context = new AppDbContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var columns = new Column[]
+                {
+                    new Column {Name = "Üres oszlop"},
+                    new Column {Name = "Egy elemes oszlop"},
+                    new Column {Name = "Sok elemes oszlop"}
+                };
+
+                context.Columns.AddRange(columns);
+                context.SaveChanges();
+
+                var todos = new Todo[]
+                {
+                    new Todo {ColumnID = 2, Position = 0, Name = "Demo 1", Deadline = DateTime.Parse("2021-12-06"), Description = "Column 2", State = TodoState.PendingState },
+                    new Todo {ColumnID = 3, Position = 0, Name = "Demo 2", Deadline = DateTime.Parse("2021-12-07"), Description = "Column 3", State = TodoState.PendingState },
+                    new Todo {ColumnID = 3, Position = 1, Name = "Demo 3", Deadline = DateTime.Parse("2021-12-08"), Description = "Column 3", State = TodoState.ProgressState },
+                    new Todo {ColumnID = 3, Position = 2, Name = "Demo 4", Deadline = DateTime.Parse("2021-12-09"), Description = "Column 3", State = TodoState.DoneState },
+                    new Todo {ColumnID = 3, Position = 3, Name = "Demo 5", Deadline = DateTime.Parse("2021-12-10"), Description = "Column 3", State = TodoState.PostponedState }
+                };
+
+                context.Todos.AddRange(todos);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ backend-test/SqliteTodoControllerTest .cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using temalabor_2021.Controllers;
+using temalabor_2021.DAL;
+using temalabor_2021.Data;
+using Xunit;
+
+namespace temalabor_2021.Tests
+{
+    public class SqliteTodoControllerTest : ControllerTest
+    {
+        public SqliteTodoControllerTest() : base(new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite("Filename=Test.db")
+            .Options)
+        {
+        }
+
+        [Fact]
+        public void CanGetAllTodoItems()
+        {
+            using (var context = new AppDbContext(ContextOptions))
+            {
+                var repo = new TodoRepository(context);
+                var controller = new TodoController(repo);
+
+                var items = controller.GetAll();
+
+                Assert.Equal(5, items.ToList().Count);
+            }
+        }
+    }
+}
